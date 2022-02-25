6a1358f4cf4ab12bf2dadfedcd3fd644fb222b10
Prokyonn <daniel@sulu.io>
2022-02-25 04:15:37 +0100

    Add tests

--- Adapter/ImageMediaAdapter.php
+++ Adapter/ImageMediaAdapter.php
@@ -22,13 +22,11 @@ use Sulu\Bundle\MediaBundle\Entity\Media;
 use Sulu\Bundle\MediaBundle\Entity\MediaInterface;
 use Sulu\Bundle\MediaBundle\Entity\MediaType;
 use Sulu\Bundle\MediaBundle\Media\Storage\StorageInterface;
-use Sulu\Bundle\SyliusConsumerBundle\Exception\ImageDownloadFailedException;
 use Sulu\Bundle\SyliusConsumerBundle\Payload\ImagePayload;
 use Sulu\Bundle\SyliusConsumerBundle\Repository\ImageMediaBridgeRepositoryInterface;
+use Sulu\Bundle\SyliusConsumerBundle\Service\SyliusImageDownloaderInterface;
 use Sulu\Component\Media\SystemCollections\SystemCollectionManagerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ImageMediaAdapter implements ImageAdapterInterface
 {
@@ -45,11 +43,6 @@ class ImageMediaAdapter implements ImageAdapterInterface
     private $entityManager;
 
     /**
-     * @var HttpClientInterface
-     */
-    private $httpClient;
-
-    /**
      * @var StorageInterface
      */
     private $storage;
@@ -62,29 +55,27 @@ class ImageMediaAdapter implements ImageAdapterInterface
     /**
      * @var string
      */
-    private $syliusBaseUrl;
+    private $collectionKey;
 
     /**
-     * @var string
+     * @var SyliusImageDownloaderInterface
      */
-    private $collectionKey;
+    private $syliusImageDownloader;
 
     public function __construct(
         ImageMediaBridgeRepositoryInterface $mediaBridgeRepository,
         EntityManagerInterface $entityManager,
-        HttpClientInterface $client,
         StorageInterface $storage,
         SystemCollectionManagerInterface $systemCollectionManager,
-        string $syliusBaseUrl,
+        SyliusImageDownloaderInterface $syliusImageDownloader,
         string $collectionKey
     ) {
         $this->mediaBridgeRepository = $mediaBridgeRepository;
         $this->entityManager = $entityManager;
-        $this->syliusBaseUrl = $syliusBaseUrl;
-        $this->httpClient = $client;
         $this->storage = $storage;
         $this->systemCollectionManager = $systemCollectionManager;
         $this->collectionKey = $collectionKey;
+        $this->syliusImageDownloader = $syliusImageDownloader;
     }
 
     public function synchronize(ImagePayload $payload): void
@@ -97,6 +88,13 @@ class ImageMediaAdapter implements ImageAdapterInterface
 
     public function remove(int $id): void
     {
+        $bridge = $this->mediaBridgeRepository->findById($id);
+        if ($bridge) {
+            $this->entityManager->remove($bridge->getMedia());
+
+            return;
+        }
+
         $this->mediaBridgeRepository->removeById($id);
     }
 
@@ -106,12 +104,12 @@ class ImageMediaAdapter implements ImageAdapterInterface
         if (!$bridge) {
             $media = new Media();
             $bridge = $this->mediaBridgeRepository->create($payload->getId(), $media);
-            $this->mediaBridgeRepository->add($bridge);
         }
 
         $media = $bridge->getMedia();
-        $uploadedFile = $this->downloadImage($payload);
+        $uploadedFile = $this->syliusImageDownloader->downloadImage($payload);
         $this->updateMedia($media, $uploadedFile, $payload->getLocale());
+        $this->entityManager->persist($bridge);
     }
 
     private function updateMedia(MediaInterface $media, UploadedFile $uploadedFile, string $locale): MediaInterface
@@ -163,27 +161,6 @@ class ImageMediaAdapter implements ImageAdapterInterface
         $this->entityManager->persist($media);
 
         return $media;
-    }
-
-    private function downloadImage(ImagePayload $payload): UploadedFile
-    {
-        $url =
-            rtrim($this->syliusBaseUrl, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR .
-            'media' . DIRECTORY_SEPARATOR . 'image' . DIRECTORY_SEPARATOR . $payload->getPath();
-        $urlParts = pathinfo($url);
-        $filename = $urlParts['filename'] . '.' . ($urlParts['extension'] ?? '');
-        $imagePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $filename;
-
-        $response = $this->httpClient->request('GET', $url);
-        if (Response::HTTP_OK !== $response->getStatusCode()) {
-            throw new ImageDownloadFailedException($url);
-        }
-        $fileHandler = fopen($imagePath, 'wb');
-        foreach ($this->httpClient->stream($response) as $chunk) {
-            fwrite($fileHandler, $chunk->getContent());
-        }
-
-        return new UploadedFile($imagePath, $filename);
     }
 
     private function getImageMediaType(): MediaType
